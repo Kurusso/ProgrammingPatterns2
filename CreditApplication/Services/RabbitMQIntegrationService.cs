4fb0ed1565d9152948ba665f941df5a13640afbb
Pavel Kupert <disturbedelk@gmail.com>
2024-04-08 19:54:51 +0700

    rabbit integration service uncommited changes

--- CreditApplication/Services/RabbitMQIntegrationService.cs
+++ CreditApplication/Services/RabbitMQIntegrationService.cs
@@ -19,11 +19,13 @@ namespace CreditApplication.Services
         public string Message { get; set; }
     }
 
-    public class RabbitMQIntegrationService : IRabbitMqService
+    public class RabbitMQIntegrationService : IRabbitMqService, IDisposable
     {
         private readonly string _rabbitMQConnection;
         private readonly string _queueTransactions;
         private readonly JsonSerializerOptions _jsonSerializerOptions;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
 
         public RabbitMQIntegrationService(IConfiguration configuration, IOptions<JsonSerializerOptions> jsonSerializerOptions)
         {
@@ -31,6 +33,14 @@ namespace CreditApplication.Services
             _rabbitMQConnection = section["Connection"];
             _queueTransactions = section["TransactionQueue"];
             _jsonSerializerOptions = jsonSerializerOptions.Value;
+            var factory = new ConnectionFactory() { Uri = new Uri(_rabbitMQConnection) };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: _queueTransactions,
+                               durable: false,
+                               exclusive: false,
+                               autoDelete: false,
+                               arguments: null);
         }
 
         public void SendMessage(object obj)
@@ -41,22 +51,17 @@ namespace CreditApplication.Services
 
         public void SendMessage(string message)
         {
-            var factory = new ConnectionFactory() { Uri = new Uri(_rabbitMQConnection) };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: _queueTransactions,
-                               durable: false,
-                               exclusive: false,
-                               autoDelete: false,
-                               arguments: null);
+            var body = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(exchange: "",
+                       routingKey: _queueTransactions,
+                       basicProperties: null,
+                       body: body);
+        }
 
-                var body = Encoding.UTF8.GetBytes(message);
-                channel.BasicPublish(exchange: "",
-                           routingKey: _queueTransactions,
-                           basicProperties: null,
-                           body: body);
-            }
+        public void Dispose()
+        {
+            _channel.Dispose();
+            _connection.Dispose();
         }
     }
 }
