4ceb5f30c8c06cfdd548e63f4a93f4058db81d05
Artem Trushkin <at.120@ya.ru>
2024-05-04 21:41:28 +0700

    wip

--- Common/Helpers/IdempotentAutoRetryHttpClient.cs
+++ Common/Helpers/IdempotentAutoRetryHttpClient.cs
@@ -1,32 +1,47 @@
 
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Common.Services;
 
 public class IdempotentAutoRetryHttpMessageHandler : DelegatingHandler
-{    
+{
     public IdempotentAutoRetryHttpMessageHandler()
-    {   
+    {
         base.InnerHandler = new HttpClientHandler();
     }
 
     protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        var idempotenceId = Guid.NewGuid().ToString(); 
+        var idempotenceId = Guid.NewGuid().ToString();
         var newUrl = QueryHelpers.AddQueryString(request.RequestUri.ToString(), "idempotenceId", idempotenceId);
         request.RequestUri = new Uri(newUrl);
-        
+
         return base.Send(request, cancellationToken);
     }
 
     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        var idempotenceId = Guid.NewGuid().ToString(); 
+        var idempotenceId = Guid.NewGuid().ToString();
         var newUrl = QueryHelpers.AddQueryString(request.RequestUri.ToString(), "idempotenceId", idempotenceId);
         request.RequestUri = new Uri(newUrl);
 
-
         return base.SendAsync(request, cancellationToken);
+    }
+}
+
+public static class IdempotentAutoRetryHttpClient
+{
+    public static void AddIdempotentAutoRetryHttpClient(this WebApplicationBuilder builder)
+    {
+
+        builder.Services.AddScoped<HttpClient>(options =>
+        {
+            // builder.Services.
+            var messageHandler = new IdempotentAutoRetryHttpMessageHandler();
+            return new HttpClient(messageHandler);
+        });
     }
 }
---
+++ UserSettings/Migrations/20240410071823_full.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserSettings.Migrations
+{
+    /// <inheritdoc />
+    public partial class full : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Theme = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HiddenAccount",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HiddenAccount", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HiddenAccount_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HiddenAccount_UserId",
+                table: "HiddenAccount",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HiddenAccount");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
